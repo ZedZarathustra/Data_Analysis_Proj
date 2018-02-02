94e9e78740f6862159f314f9dc3443418fe84ccd
Scott Zwick <sdzwick@nppd.com>
2018-02-02 13:29:43 -0600

    Subset by Baltimore and type

---
+++ Plot3.R
@@ -0,0 +1,47 @@
+## Exploratory_Data_Analysis - Course Project - Plot 2
+## 
+## Of the four types of sources indicated by the type (point, nonpoint,
+## onroad, nonroad) variable, which of these four sources have seen decreases
+## in emissions from 1999–2008 for Baltimore City(fips == 24510)? Which have
+## seen increases in emissions from 1999–2008? Use the ggplot2plotting
+## system to make a plot answer this question.
+##
+## Author: Scott D. Zwick, 2018/01/31
+
+setwd("C:/Users/sdzwick/Documents/R/Coursera/4-Exploratory_Data_Analysis/Data_Analysis_Proj")
+## read files
+summPM25 <- readRDS("summarySCC_PM25.rds")
+## This file contains a data frame with all of the PM2.5 emissions data for
+## 1999, 2002, 2005, and 2008. For each year, the table contains number
+## of tons of PM2.5 emitted from a specific type of source for the
+## entire year.
+## 
+## fips: A five-digit number (represented as a string) indicating the
+## U.S. county
+## SCC: The name of the source as indicated by a digit string (see
+## source code classification table)
+## Pollutant: A string indicating the pollutant
+## Emissions: Amount of PM2.5 emitted, in tons
+## type: The type of source (point, non-point, on-road, or non-road)
+## year: The year of emissions recorded
+
+SrcClsCode <- readRDS("Source_Classification_Code.rds")
+## This table provides a mapping from the SCC digit strings in the
+## Emissions table to the actual name of the PM2.5 source. The sources
+## are categorized in a few different ways from more general to more
+## specific and you may choose to explore whatever categories you think
+## are most useful. For example, source “10100101” is known as
+## “Ext Comb /Electric Gen /Anthracite Coal /Pulverized Coal”.
+
+
+## Subset Baltimore City, Maryland
+PM25_Balt <- subset(summPM25, summPM25$fips==24510)
+
+## Subset by type
+PM_25_Balt_pt <- subset(PM25_Balt, PM25_Balt$type=="POINT")
+PM_25_Balt_npt <- subset(PM25_Balt, PM25_Balt$type=="NONPOINT")
+PM_25_Balt_rd <- subset(PM25_Balt, PM25_Balt$type=="ON-ROAD")
+PM_25_Balt_nrd <- subset(PM25_Balt, PM25_Balt$type=="NON-ROAD")
+
+## Calculate total for each year
+PM25_Balt_tot<- tapply(PM25_Balt$Emissions, PM25_Balt$year, PM25_Balt$type, sum)
